461dc145808e84d93f79c771e62981e971d1a693
Utkarsh Gupta <34169292+Uttu316@users.noreply.github.com>
2025-01-31 20:30:19 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -6,7 +6,7 @@ const WeatherDisplay = ({data})=>{
     const {temperature,conditions} = data;
   return (
   <div>
-    <p>Temperature: {temperature}</p><span style={{color:temperature>20:"rgb(255, 0, 0)":"blue"}}></span>
+    <p>Temperature: {temperature}</p><span style={{color:temperature>20?"rgb(255, 0, 0)":"blue"}}></span>
 
     <p>Conditions: {conditions}</p>
     </div>
